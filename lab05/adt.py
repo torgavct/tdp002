d9d1a49e125db9e27dd332b4a0ff0ccc3f628f3a
torgav <christian.torgav@gmail.com>
2024-10-16 03:37:30 +0200

    move

--- lab05/adt.py
+++ lab05/adt.py
@@ -1,3 +1,5 @@
+import os
+
 def create_board():
     return []
     
@@ -18,14 +20,14 @@ def add_storage(board,x,y):
 def add_space(board,x,y):
     board.append([' ',x,y])
 
-def get_coords(board,x,y):
+def get_position(board,x,y):
     data = board_data(board)
-    y = y*get_max(board,'y')
-    for index in range(y,(y+get_max(board,'y')  )):
-        if x == index:
-            symbol = data[index]
-            return symbol
-
+    iteration = 0
+    for i in range(0,len(data),get_max(board,'x')):
+        chunk = data[i:i+get_max(board,'y')]
+        if iteration == y:
+            return chunk[x]
+        iteration+=1
 
 def get_max(board, n):
     if n == 'x': n = 1
@@ -43,7 +45,7 @@ def board_data(board):
     row = 0
     small_list = []
     while row < get_max(board, 'y'):
-        for i in range(get_max(board,'x')): 
+        for i in range(get_max(board,'y')): 
             small_list.append(' ')
         for object in board:
             if object[2] == row:
@@ -52,3 +54,51 @@ def board_data(board):
         data = data + small_list
         small_list.clear()
     return data
+
+def move_box(board, x, y):
+    print("move box")
+    pass
+
+def player_direction(board, dir):
+    if dir.lower() == 'w':
+        move(0,-1, board)
+           
+    elif dir.lower() == 'a':
+        move(-1,0,board)
+            
+    elif dir.lower() == 's':
+        move(0,1,board)
+            
+    elif dir.lower() == 'd':
+        move(1,0,board)
+           
+    
+def move(dir_x, dir_y, board):
+    player = player_pos(board)
+    new_x = player[1]+dir_x
+    new_y = player[2]+dir_y
+    try:
+        new_pos = get_position(board,new_x,new_y)
+    except:
+        return board
+    if new_pos == ' ':
+        remove_item(board, player[1], player[2])
+        add_player(board,new_x,new_y)
+        return board
+    elif new_pos == '0':
+        move_box(board,)
+    return board
+
+def player_pos(board):
+    for object in board:
+        if object[0] == '@':
+            return object
+    print("NO PLAYER FOUND")
+
+def remove_item(board, x, y):
+    for index in board:
+        if index[1] == x and index[2] == y:
+            item = board.index(index)
+            board.pop(item)
+            return board
+    
--- lab05/sokoban.py
+++ lab05/sokoban.py
@@ -1,6 +1,11 @@
+import os
+from time import sleep
 from adt import *
 
-def sokoban_display():
+def sokoban_display(board):
+    sleep(0)
+    os.system('clear')
+    print("#############")
     data = board_data(board)
     column = 0 
     edge = get_max(board,'x')
@@ -10,21 +15,27 @@ def sokoban_display():
         if column == edge:
             print()
             column = 0
-
- def open_sokoban():           
+            
+    print("############")
+def open_sokoban():   
+    pass       
             
       
         
 
-        
+def play(): 
+    board = create_board()          
+
+    add_player(board, 0, 0)
     
-           
-board = create_board()          
-add_wall(board, x = 1, y = 0)
-add_wall(board, x = 0, y = 1)
-add_wall(board, x = 2, y = 1)
-add_wall(board, x = 1, y = 2)
-add_player(board, 1, 1)
-sokoban_display()
-coords = get_coords(board, 1, 1)
-print(coords)
+    while True:   
+        sokoban_display(board)
+        dir = input(" ")
+        player_direction(board, dir)
+        print(board_data(board))
+        if dir == "exit":
+            break
+  
+    
+    
+play()
